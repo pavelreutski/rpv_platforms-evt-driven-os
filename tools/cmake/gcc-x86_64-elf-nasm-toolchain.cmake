d3617deb97b13fbfaa5bdce85ccdc2024f6c6217
pavelreutski <pavel.reutsky@gmail.com>
2025-12-11 23:49:27 +0100

    more house clean keeping

--- tools/cmake/gcc-x86_64-elf-nasm-toolchain.cmake
+++ tools/cmake/gcc-x86_64-elf-nasm-toolchain.cmake
@@ -23,40 +23,12 @@ set(CMAKE_OBJDUMP      ${CROSS}-objdump)
 set(CMAKE_RANLIB       ${CROSS}-ranlib)
 set(CMAKE_READELF      ${CROSS}-readelf)
 
-# ------------------------------
-# Enable ASM_NASM
-# ------------------------------
-set(CMAKE_ASM_NASM_OBJECT_FORMAT elf64)
-set(CMAKE_ASM_NASM_FLAGS "-f elf64 -g -F dwarf")
-
 # ---------------------
 # Global compiler flags
 # ---------------------
 
 add_compile_options(
-    $<$<COMPILE_LANGUAGE:C>:-m64>
-    $<$<COMPILE_LANGUAGE:C>:-ffreestanding>
-    $<$<COMPILE_LANGUAGE:C>:-fno-pic>
-    $<$<COMPILE_LANGUAGE:C>:-fno-pie>
-    $<$<COMPILE_LANGUAGE:C>:-fno-omit-frame-pointer>
-    $<$<COMPILE_LANGUAGE:C>:-fno-stack-protector>
-    $<$<COMPILE_LANGUAGE:C>:-fno-strict-aliasing>
-    $<$<COMPILE_LANGUAGE:C>:-O2>
-    $<$<COMPILE_LANGUAGE:C>:-g>
-    $<$<COMPILE_LANGUAGE:C>:-Wall>
-    $<$<COMPILE_LANGUAGE:C>:-Wextra>
-)
-
-# ---------------------
-# Linker flags
-# ---------------------
-
-add_link_options(-nostdlib
-                -nostartfiles
-                -nodefaultlibs
-                -static
-                -zmax-page-size=0x1000
-                -znoexecstack)
+    "$<$<COMPILE_LANGUAGE:C>:-m64;-Wall;-Wextra>")
 
 # Prevent CMake from adding system libs
 set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
